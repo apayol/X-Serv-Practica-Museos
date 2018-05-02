5a4df94fdfc7a4d9e10b1f9ec8be4fe9a173b2a1
apayol <apayolmontero@gmail.com>
2018-05-02 20:06:37 +0200

    Template añadido, aún no incorporado. Link descarga del xml añadido

--- myfinalproject/museos/views.py
+++ myfinalproject/museos/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template.loader import get_template
+from django.template import Context
 
 # Create your views here.
 
@@ -23,3 +25,10 @@ def login_exito (request):
     respuesta = "Ha entrado como <b>" + request.user.username
     respuesta += "</b> exitosamente."
     return HttpResponse(respuesta)
+
+# Parsear de este link el xml, guardarlo en mi base de datos
+    link = 'https://datos.madrid.es/portal/site/egob/menuitem.c05'
+    link += 'c1f754a33a9fbe4b2e4b284f1a5a0/?vgnextoid=118f2fdbec'
+    link += 'c63410VgnVCM1000000b205a0aRCRD&vgnextchannel=374512'
+    link += 'b9ace9f310VgnVCM100000171f5a0aRCRD&vgnextfmt=default'
+
